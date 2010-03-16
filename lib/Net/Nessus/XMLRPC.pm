7e1e303abb611c953da65a007581b954c256dbbd
Vlatko Kosturjak <kost@linux.hr>
2010-03-16 08:50:10 +0100

    Version bump to 0.03

--- lib/Net/Nessus/XMLRPC.pm
+++ lib/Net/Nessus/XMLRPC.pm
@@ -13,11 +13,11 @@ Net::Nessus::XMLRPC - Communicate with Nessus scanner(v4.2+) via XMLRPC
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
